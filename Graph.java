9fd32f593fab19eb2a911ca3a2c790c579d33ba6
FTPaiva <felipetakasawapaiva@gmail.com>
2021-12-19 20:57:41 -0300

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -3,23 +3,39 @@ import java.io.FileNotFoundException;  // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Graph {
+    boolean isMatrix;
+    int vertexCount;
 
-    static void ReadInputFile() {
+    public Graph(boolean representation) {
+        isMatrix = representation;
+    }
+
+    private void ReadInputFile() {
         try {
             File myObj = new File("Input.txt");
             Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+
+            vertexCount = Integer.valueOf(myReader.nextLine()); // Reads vertex count from input file
+
+            while (myReader.hasNextLine()) { // Reads and creates edges from input file
+                String edge [] = myReader.nextLine().split(" ");
+                AddEdge(Integer.valueOf(edge[0]), Integer.valueOf(edge[0]));
             }
+
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
+
+    private void AddEdge(int vertex1, int vertex2) {
+
+    }
+
     public static void main(String[] args) {
-        ReadInputFile();
+        Graph myGraph = new Graph(true);
+        myGraph.ReadInputFile();
     }
 
 

65325219b73402cf5c720e129099fff1598fba71
FTPaiva <felipetakasawapaiva@gmail.com>
2022-02-16 21:54:10 -0300

    low cost dijkstra + mst5

--- WGraph.java
+++ WGraph.java
@@ -389,6 +389,71 @@ public class WGraph {
         
     }
 
+    private void LowCostDijkstra(int s, int target) {
+        int[] parents = new int[vertexCount]; // Array containing the parent of each node
+        Boolean explored[] = new Boolean[vertexCount]; // Array that represents what vertices were explored
+        float dist[] = new float[vertexCount]; // Array containing the current distance estimate of each vertex
+
+        for (int i = 0; i < vertexCount; i++) { // Sets all vertices as not explored and all the distances to infinite
+            explored[i] = false;
+            dist[i] = Float.MAX_VALUE;
+        }
+        dist[s-1] = 0; // Distance from source to itself is set to 0
+        parents[s-1] = -1;
+
+        for (int i = 0; i < vertexCount; i++) { // Goes through all vertices in the graph
+            int u = MinDistance(dist, explored); // Chooses the not yet explored vertex with minimum distance
+            System.out.println(u);
+
+            explored[u] = true;
+
+            for (Pair<Integer, Float> edge : array.get(u)) {
+                if (!explored[edge.getKey()-1] && dist[u] != Float.MAX_VALUE && dist[u] + edge.getValue() < dist[edge.getKey()-1]) {
+                    dist[edge.getKey()-1] = dist[u] + edge.getValue();
+                    parents[edge.getKey()-1] = u + 1;
+                }
+            }
+
+        }
+        /*
+        System.out.println("distances: ");
+        for (float i : dist) {
+            System.out.println(i);
+        }
+        System.out.println("Parents: ");
+        for (int i : parents) {
+            System.out.println(i);
+        }
+        System.out.println("Path: ");
+        for (Integer i : GetPath(4, parents)) {
+            System.out.println(i);
+        }*/
+        if (target != -1) {
+            System.out.println("\nDistance between vertices " + target + " and " + (s) + " = " + dist[target-1]);
+            System.out.print("Path:");
+            for (Integer node : GetPathDijkstra(target, parents)) {
+                System.out.print(" > "+ (node));
+            }
+            System.out.print("\n\n");
+        } else {
+            System.out.println("\nDistances between vertex " + (s) + " and all vertices, along with their paths:\n");
+            for (int vertex = 1; vertex <= vertexCount; vertex++) {
+                //if (vertex == 10 || vertex == 20 || vertex == 30 || vertex == 40 || vertex == 50){
+                System.out.println("Distance to vertex " + (vertex) + " = " + dist[vertex-1]);
+                System.out.print("Path to vertex " + (vertex) + ": ");
+                for (Integer node : GetPathDijkstra(vertex, parents)) {
+                    System.out.print(" > "+ (node));
+                }
+                System.out.print("\n\n");
+                //}
+            }
+        }
+        
+
+
+        
+    }
+
     private float [] BellmanFord(int target, int start) {
         System.out.println("BellmanFord started running!");
 
